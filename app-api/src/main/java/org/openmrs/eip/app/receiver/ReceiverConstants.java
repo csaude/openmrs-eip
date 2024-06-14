1854ef4feb5f97c69eaaa549341e3ac7f8fd47ff
wluyima <wyclif@openmrs.org>
2024-06-14 15:06:43 +0300

    EC-680 - Add property to disable receiving new messages from the broker

--- app-api/src/main/java/org/openmrs/eip/app/receiver/ReceiverConstants.java
+++ app-api/src/main/java/org/openmrs/eip/app/receiver/ReceiverConstants.java
@@ -54,6 +54,8 @@ public class ReceiverConstants {
 	
 	public static final String PROP_SYNC_ORDER_BY_ID = "sync.order.by.id";
 	
+	public static final String PROP_JMS_LISTENER_DISABLED = "jms.msg.listener.disabled";
+	
 	public static final String PROP_JMS_SKIP_DUPLICATES = "jms.skip.duplicates";
 	
 	public static final String ROUTE_ID_MSG_PROCESSOR = "message-processor";
