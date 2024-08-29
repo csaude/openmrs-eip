47736d605cac4cad1638c920931572fd0cd81b20
wluyima <wyclif@openmrs.org>
2024-08-29 18:07:14 +0300

    EC-723 - Added base class to move items and modified SyncedMessageArchiver to use it

--- app-api/src/main/java/org/openmrs/eip/app/receiver/SyncedMessageArchiver.java
+++ app-api/src/main/java/org/openmrs/eip/app/receiver/SyncedMessageArchiver.java
@@ -1,8 +1,11 @@
 package org.openmrs.eip.app.receiver;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
-import org.openmrs.eip.app.management.entity.receiver.SiteInfo;
+import org.openmrs.eip.app.AppUtils;
+import org.openmrs.eip.app.BaseMovingTask;
 import org.openmrs.eip.app.management.entity.receiver.SyncedMessage;
 import org.openmrs.eip.app.management.repository.SyncedMessageRepository;
 import org.openmrs.eip.component.SyncContext;
@@ -13,18 +16,18 @@ import org.slf4j.LoggerFactory;
  * Reads and processes done messages in the synced message queue and moves them to the archives
  * queue
  */
-public class SyncedMessageArchiver extends BaseSiteRunnable {
+public class SyncedMessageArchiver extends BaseMovingTask<SyncedMessage> {
 	
 	protected static final Logger log = LoggerFactory.getLogger(SyncedMessageArchiver.class);
 	
+	protected static final String ARCHIVE_INSERT = "INSERT INTO receiver_sync_archive (model_class_name,identifier,"
+	        + "entity_payload,site_id,is_snapshot,message_uuid,date_sent_by_sender,operation,date_created,"
+	        + "date_received,sync_version) VALUES (?,?,?,?,?,?,?,?,now(),?,?)";
+	
 	private SyncedMessageRepository syncedMsgRepo;
 	
-	private SyncedMessageArchivingProcessor processor;
-	
-	public SyncedMessageArchiver(SiteInfo site) {
-		super(site);
+	public SyncedMessageArchiver() {
 		syncedMsgRepo = SyncContext.getBean(SyncedMessageRepository.class);
-		processor = SyncContext.getBean(SyncedMessageArchivingProcessor.class);
 	}
 	
 	@Override
@@ -33,28 +36,30 @@ public class SyncedMessageArchiver extends BaseSiteRunnable {
 	}
 	
 	@Override
-	public boolean doRun() throws Exception {
+	public List<SyncedMessage> getNextBatch() {
 		if (log.isTraceEnabled()) {
-			log.trace("Fetching next batch of " + page.getPageSize() + " messages to archive for site: " + site);
+			log.trace("Fetching next batch of " + AppUtils.getTaskPage().getPageSize() + " synced items to archive");
 		}
 		
-		List<SyncedMessage> messages = syncedMsgRepo.getBatchOfMessagesForArchiving(site, page);
-		
-		if (messages.isEmpty()) {
-			if (log.isTraceEnabled()) {
-				log.trace("No messages for archiving found for site: " + site);
-			}
-			
-			return true;
-		}
-		
-		if (log.isDebugEnabled()) {
-			log.debug("Archiving " + messages.size() + " message(s) for site: " + site);
-		}
-		
-		processor.processWork(messages);
-		
-		return true;
+		return syncedMsgRepo.getBatchOfMessagesForArchiving(AppUtils.getTaskPage());
 	}
 	
+	@Override
+	protected String getInsertQuery() {
+		return ARCHIVE_INSERT;
+	}
+	
+	@Override
+	protected void addItem(PreparedStatement insertStatement, SyncedMessage item) throws SQLException {
+		insertStatement.setString(1, item.getModelClassName());
+		insertStatement.setString(2, item.getIdentifier());
+		insertStatement.setString(3, item.getEntityPayload());
+		insertStatement.setLong(4, item.getSite().getId());
+		insertStatement.setBoolean(5, item.getSnapshot());
+		insertStatement.setString(6, item.getMessageUuid());
+		insertStatement.setObject(7, item.getDateSentBySender());
+		insertStatement.setString(8, item.getOperation().name());
+		insertStatement.setObject(9, item.getDateReceived());
+		insertStatement.setString(10, item.getSyncVersion());
+	}
 }
