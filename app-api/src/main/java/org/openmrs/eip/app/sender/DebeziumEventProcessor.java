4609d73f575969d92ea6b5f8123538200e53e485
wluyima <wyclif@openmrs.org>
2024-09-10 13:30:18 +0300

    EC-683 - Sender should squash events for the same row

--- app-api/src/main/java/org/openmrs/eip/app/sender/DebeziumEventProcessor.java
+++ app-api/src/main/java/org/openmrs/eip/app/sender/DebeziumEventProcessor.java
@@ -2,15 +2,22 @@ package org.openmrs.eip.app.sender;
 
 import static org.openmrs.eip.app.SyncConstants.BEAN_NAME_SYNC_EXECUTOR;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import org.apache.camel.ProducerTemplate;
 import org.apache.commons.lang3.StringUtils;
 import org.openmrs.eip.app.BaseFromCamelToCamelEndpointProcessor;
 import org.openmrs.eip.app.management.entity.sender.DebeziumEvent;
+import org.openmrs.eip.app.management.repository.DebeziumEventRepository;
+import org.openmrs.eip.component.SyncOperation;
 import org.openmrs.eip.component.SyncProfiles;
 import org.openmrs.eip.component.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -19,9 +26,14 @@ import org.springframework.stereotype.Component;
 @Profile(SyncProfiles.SENDER)
 public class DebeziumEventProcessor extends BaseFromCamelToCamelEndpointProcessor<DebeziumEvent> {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(DebeziumEventProcessor.class);
+	
+	private DebeziumEventRepository repo;
+	
 	public DebeziumEventProcessor(ProducerTemplate producerTemplate,
-	    @Qualifier(BEAN_NAME_SYNC_EXECUTOR) ThreadPoolExecutor executor) {
+	    @Qualifier(BEAN_NAME_SYNC_EXECUTOR) ThreadPoolExecutor executor, DebeziumEventRepository repo) {
 		super(SenderConstants.URI_DBZM_EVENT_PROCESSOR, producerTemplate, executor);
+		this.repo = repo;
 	}
 	
 	@Override
@@ -57,6 +69,42 @@ public class DebeziumEventProcessor extends BaseFromCamelToCamelEndpointProcesso
 	@Override
 	public List<String> getLogicalTypeHierarchy(String logicalType) {
 		return Utils.getListOfTablesInHierarchy(logicalType);
+	}
+	
+	@Override
+	public void processWork(List<DebeziumEvent> items) throws Exception {
+		//Squash events for the same row so that exactly one message is processed in case of multiple in this run in.
+		//Delete being a terminal event, squash for a single entity will stop at the last event before a delete event
+		//to ensure we don't re-process a non-existent entity
+		Map<String, DebeziumEvent> keyAndEarliestMsgMap = new LinkedHashMap(items.size());
+		List<DebeziumEvent> squashedEvents = new ArrayList();
+		items.stream().forEach(dbzmEvent -> {
+			String table = dbzmEvent.getEvent().getTableName();
+			String key = table + "#" + dbzmEvent.getEvent().getPrimaryKeyId();
+			if (!keyAndEarliestMsgMap.containsKey(key)) {
+				keyAndEarliestMsgMap.put(key, dbzmEvent);
+			} else {
+				if (dbzmEvent.getEvent().getOperation() != SyncOperation.d.name()) {
+					squashedEvents.add(dbzmEvent);
+					
+					if (LOG.isTraceEnabled()) {
+						LOG.trace("Squashing entity event -> " + dbzmEvent);
+					}
+				} else {
+					if (LOG.isTraceEnabled()) {
+						LOG.trace(
+						    "Squashing stopped for " + key + ", postponing processing of delete event -> " + dbzmEvent);
+					}
+				}
+			}
+		});
+		
+		doProcessWork(keyAndEarliestMsgMap.values().stream().toList());
+		repo.deleteAll(squashedEvents);
+	}
+	
+	protected void doProcessWork(List<DebeziumEvent> items) throws Exception {
+		super.processWork(items);
 	}
 	
 }
--- app-api/src/test/java/org/openmrs/eip/app/sender/DebeziumEventProcessorTest.java
+++ app-api/src/test/java/org/openmrs/eip/app/sender/DebeziumEventProcessorTest.java
@@ -24,7 +24,7 @@ public class DebeziumEventProcessorTest {
 	@Before
 	public void setup() {
 		Whitebox.setInternalState(BaseQueueProcessor.class, "initialized", true);
-		processor = new DebeziumEventProcessor(null, null);
+		processor = new DebeziumEventProcessor(null, null, null);
 	}
 	
 	@After
